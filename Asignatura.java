1b65eebdb42791f082e604e513e382b67234b396
P4bl0AGT <paguileratapia17@gmail.com>
2024-08-25 15:16:28 -0400

    Update Asignatura.java

---
+++ Asignatura.java
@@ -0,0 +1,51 @@
+public class Asignatura
+{
+    //ATRIBUTOS
+    private String nombre;
+    private String codigo;
+    private String profesor;
+    private int creditos;
+
+    //CONSTRUCTOR
+    public Asignatura(){}
+    public Asignatura(String nombre, String codigo, String profesor, int creditos)
+    {
+        setNombre(nombre);
+        setCodigo(codigo);
+        setProfesor(profesor);
+        setCreditos(creditos);
+    }
+
+    //SETTERS
+    public void setNombre(String nombre)
+    {
+        if(nombre != null)
+            this.nombre = nombre;
+        else
+            this.nombre = "nulo";
+    }
+
+    public void setCodigo(String codigo)
+    {
+        if(codigo != null)
+            this.codigo = codigo;
+        else
+            this.codigo = "nulo";
+    }
+
+    public void setProfesor(String profesor)
+    {
+        if(profesor != null)
+            this.profesor = profesor;
+        else
+            this.profesor = "no hay profesor a cargo";
+    }
+
+    public void setCreditos(int creditos)
+    {
+        if(creditos >= 0)
+            this.creditos = creditos;
+        else
+            this.creditos = "nulo";
+    }
+}
