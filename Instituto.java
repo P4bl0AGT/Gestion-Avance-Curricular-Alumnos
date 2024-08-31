a2f1430ed789483da3a38632221af35a034ca73f
CRISTOBAL ANDRES RUBILAR VICENCIO <cristobal.rubilar.v@mail.pucv.cl>
2024-08-31 17:13:39 -0400

    Update Instituto.java

--- Instituto.java
+++ Instituto.java
@@ -1,3 +1,4 @@
+import java.io.*;
 import java.util.ArrayList;
 import java.util.HashMap;
 
@@ -35,4 +36,53 @@ public class Instituto
             System.out.println("");
         }
     }
+
+    public void buscarAlumnosRut(HashMap mapaAlumnos, BufferedReader lector)throws IOException{
+    
+        
+        System.out.println("PORFAVOR INGRESAR EL RUT DEL ALUMNO A BUSCAR");
+        String rut = lector.readLine();
+
+        if(mapaAlumnos.get(rut) != null){
+
+            Alumno alumnoBuscado = (Alumno)mapaAlumnos.get(rut);
+            alumnoBuscado.mostrarDetalle();
+        }
+        else{
+            System.out.println("No se encuentra alumnos con el rut ingresado");
+        }
+        
+    }
+
+    public void buscarAlumnosCarrera(ArrayList listaAlumnos,BufferedReader lector)throws IOException{
+    
+        System.out.println("PORFAVOR INGRESAR la Carrera a buscar: ");
+        String carrera = lector.readLine();
+        Alumno alumnoCarrera;
+        boolean hayAlumnosCarrera = false;
+        
+        if (cantidadAlumnos != 0)
+        {
+            for (int i = 0; i < listaAlumnos.size() ; i++)
+            {
+                alumnoCarrera = (Alumno)listaAlumnos.get(i);
+
+                if (alumnoCarrera.getCarrera().getNombre().equals(carrera))
+                {
+                    alumnoCarrera.mostrarDetalle();
+                    hayAlumnosCarrera = true;
+                }
+            }
+
+            if (!hayAlumnosCarrera)System.out.println("NO HAY ALUMNOS INSCRITOS EN ESTA CARRERA");
+
+        }
+        else{
+            System.out.println("NO HAY ALUMNOS INSCRITOS EN EL INSTITUTO");
+        }
+
+
+
+    }
+
 }
