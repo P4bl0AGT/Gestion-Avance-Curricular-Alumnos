abafc1c77052c9c77697fb1fde96f10091427fde
P4bl0AGT <paguileratapia17@gmail.com>
2024-09-24 17:20:37 -0300

    Cambios Pakcage

---
+++ src/main/java/Alumno.java
@@ -0,0 +1,99 @@
+public class Alumno extends Persona{
+
+    //ATRIBUTOS
+    private int creditosAprobados;
+    private Carrera carrera;
+
+
+    //CONSTRUCTOR
+    public Alumno(String nombre, String apellido, String rut, int edad, Carrera carrera) {
+        super(nombre, apellido, rut, edad);
+        this.carrera = carrera;
+        creditosAprobados = 0;
+    }
+
+    public Alumno(String nombre, String apellido, String rut, int edad) {
+        super(nombre, apellido, rut, edad);
+        carrera = null;
+        creditosAprobados = 0;
+    }
+    
+
+    //GETTERS
+    public Carrera getCarrera() {
+        return carrera;
+    }
+
+    public int getCreditosAprobados() {
+        return creditosAprobados;
+    }
+
+
+    //SETTERS
+    public void setCarrera(Carrera carrera) {
+        if(carrera != null ) 
+            this.carrera = carrera;
+        else
+            this.carrera = null;
+    }
+
+    public void setCreditosAprobados(int creditosAprobados){
+        if(creditosAprobados > 0)
+            this.creditosAprobados = creditosAprobados;
+        else
+            this.creditosAprobados = 0;
+    }
+
+
+    //METODOS
+    @Override
+    public void mostrar() {
+        System.out.println("Nombre del Alumno: " + getNombre());
+        System.out.println("Apellido del Alumno: " + getApellido());
+        System.out.println("Edad del Alumno: " + getEdad());
+        System.out.println("Rut del Alumno: " + getRut());
+        System.out.println("Carrera del alumno: "+ carrera.getNombre());
+        System.out.println("Creditos aprovados: "+ creditosAprobados);
+    }
+
+    @Override
+    public void mostrar(boolean simple) {
+        System.out.println(" | " + getNombre() + " | " + getRut() + " | " + carrera.getNombre() + " | ");
+    }
+
+
+    /*
+    public AsignaturaInscrita incribirAsignatura(Asignatura asignatura) {
+        String codigo = asignatura.getCodigo();
+        String nombre = asignatura.getNombre();
+        int creditos = asignatura.getCreditos();
+        Enums.estadoAsignatura estado = Enums.estadoAsignatura.INSCRITO;
+        int nota = 0;
+        AsignaturaInscrita inscrita = new AsignaturaInscrita(codigo, nombre, creditos, estado, nota);
+        return inscrita;
+    }
+    */
+
+
+    //cuidado con asignatura y asignaturainscrita en contenedor
+    public void inscribirCarrera(Carrera carrera) {
+
+        Carrera nuevaCarrera = carrera.copiaParaInscribir();
+
+        for (int i = 0; i < carrera.cantidadAsignaturas(); i++) {
+            Asignatura asignaturaActual = carrera.obtenerAsignatura(i);
+            AsignaturaInscrita inscrita = new AsignaturaInscrita();
+            inscrita.modificarParaInscribir(asignaturaActual);
+
+            nuevaCarrera.agregarAsignatura(inscrita);
+        }
+        setCarrera(nuevaCarrera);
+    }
+
+    
+    public int obtenerCantidadAsignaturas() {
+        return carrera.cantidadAsignaturas();
+    }
+
+}
+
---
+++ src/main/java/Profesor.java
@@ -0,0 +1,50 @@
+import java.util.Iterator;
+
+
+public class Profesor extends Persona{
+    //ATRIBUTOS
+    private Contenedor<String, Asignatura> asignaturas;
+
+    //CONSTRUCTOR
+    public Profesor(String nombre, String apellido, String rut, int edad) {
+        super(nombre, apellido, rut, edad);
+        asignaturas = new Contenedor<>();
+    }
+
+    //METODOS
+    @Override
+    public void mostrar() { 
+        System.out.println("Nombre del Profesor: " + getNombre());
+        System.out.println("Apellido del Profesor: " + getApellido());
+        System.out.println("Edad del Profesor: " + getEdad());
+        System.out.println("Rut del Profesor: " + getRut());
+        System.out.println("Asignaturas del Profesor: ");
+        Iterator<Asignatura> iterador = asignaturas.iterador();
+        while (iterador.hasNext()) {
+            Asignatura aux = iterador.next();
+            aux.mostrar();
+        }
+    }
+
+    @Override
+    public void mostrar(boolean simple) {
+        System.out.println(" | " + getNombre() + " | " + getRut() + " | " );
+        /* 
+        Iterator<Asignatura> iterador = asignaturas.iterador();
+        while (iterador.hasNext()) {
+            Asignatura aux = iterador.next();
+            aux.mostrar(true);
+        }
+        */
+    }
+
+    public boolean agregarAsignatura(Asignatura asignatura) {
+        return asignaturas.agregar(asignatura.getCodigo(), asignatura);
+    }
+
+    public Asignatura elimarAsignatura(String codigo) {
+        return asignaturas.eliminar(codigo);
+    }
+
+
+}
